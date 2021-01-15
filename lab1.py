413c2f70f7ee2f006ed2c243df8bd10152f6cd35
jha8 <42952573+jha8@users.noreply.github.com>
2021-01-14 19:49:04 -0700

    update script

--- lab1.py
+++ lab1.py
@@ -1,8 +1,17 @@
 import requests
+
+# References used: 
+# curl manpage
+# https://requests.readthedocs.io/en/master/ for python request library
+
 # Q2
 # print(requests.__version__)
 
 # Curl 5)
-test = requests.get('http://www.google.com')
-print(test.status_code)
+# test = requests.get('http://www.google.com')
+# print(test.status_code)
+
+# Curl 10)
+test = requests.get('https://raw.githubusercontent.com/jha8/cmputlab1/main/lab1.py')
+print(test.text)
 
